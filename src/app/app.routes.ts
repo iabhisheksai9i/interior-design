6d354775d99032b7bacea77db30139b641bd4038
iabhisheksai9i <abhisheksaini2542002@gmail.com>
2024-02-24 10:51:42 +0530

    initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'nav', pathMatch: 'full' },
+    { path: "nav", component: NavbarComponent },
+];
