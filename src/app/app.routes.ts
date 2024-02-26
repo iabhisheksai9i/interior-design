e3e45dcfdcf2997edd11082400e4b64c47a811d3
Gauravgurjar01 <gauravbhatia00786@gmail.com>
2024-02-26 13:33:43 +0530

    change in raw-material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { NavbarComponent } from '../app/pages/navbar/navbar.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { RawMaterialComponent } from './pages/raw-material/raw-material.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: "nav", component: NavbarComponent },
     { path: "home", component: HomepageComponent },
+    { path: "raw-material", component: RawMaterialComponent },
 
 ];
--- src/app/pages/raw-material/raw-material.component.ts
+++ src/app/pages/raw-material/raw-material.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+// import { CarouselModule } from 'primeng/carousel';
+// import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-raw-material',
   standalone: true,
-  imports: [],
+  // imports: [CarouselModule,ButtonModule],
   templateUrl: './raw-material.component.html',
   styleUrl: './raw-material.component.css'
 })
 export class RawMaterialComponent {
 
+//   products:any=[
+//     {
+//       image:"https://5.imimg.com/data5/ML/MU/ZQ/SELLER-8278996/glass-door-500x500.jpg"
+//     },
+//     {
+//       image:"https://i.pinimg.com/originals/05/a6/22/05a6228e4c45eaeee3f76a227cb8effc.jpg"
+//     },
+//     {
+//       image:"https://4.imimg.com/data4/JP/PO/MY-25195161/office-door-glass-500x500.jpg"
+//     },
+//     {
+//       image:"https://5.imimg.com/data5/RC/OH/HM/SELLER-96289206/single-sliding-glass-door.jpg"
+//     },
+// ]
 }
