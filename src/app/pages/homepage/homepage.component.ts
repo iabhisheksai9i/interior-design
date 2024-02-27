4b851ea477a7254dd477df2ffdc1cdd142146e6e
iabhisheksai9i <abhisheksaini2542002@gmail.com>
2024-02-27 09:58:35 +0530

    change in navbar

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
 import { CarouselModule } from 'primeng/carousel';
 import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [CarouselModule,ButtonModule],
+  imports: [CarouselModule,ButtonModule,DialogModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
 export class HomepageComponent {
 
+  visible:boolean=false;
+
   products:any=[
     {
-      image:"https://5.imimg.com/data5/ML/MU/ZQ/SELLER-8278996/glass-door-500x500.jpg"
+      image:"https://5.imimg.com/data5/ML/MU/ZQ/SELLER-8278996/glass-door-500x500.jpg",
+      name:'Dore with glass'
     },
     {
       image:"https://i.pinimg.com/originals/05/a6/22/05a6228e4c45eaeee3f76a227cb8effc.jpg"
@@ -53,5 +58,10 @@ export class HomepageComponent {
     }
   ]
 
+  showDialog(){
+    this.visible=true
+  }
+
 }
+
 
