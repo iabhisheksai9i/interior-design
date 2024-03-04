06cf206d375a241f7ff7aa8918cb4e4f1ea6ecc8
Gauravgurjar01 <gauravbhatia00786@gmail.com>
2024-03-04 16:23:50 +0530

    change-in-feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,21 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
-import { FeatureComponent } from './pages/raw-material/feature/feature.component';
 import { RawMaterialComponent } from './pages/raw-material/raw-material/raw-material.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: "home", component: HomepageComponent },
-    { path: "raw-material", component: RawMaterialComponent },
-    { path: `raw-material/:id/feature`, component:FeatureComponent },
+    { path: "raw-material",
+    children:[
+        {path:'', component: RawMaterialComponent,pathMatch: 'full'},
+        {
+        path:':id/feature',
+        loadComponent:()=>import('./pages/raw-material/feature/feature.component').then(c=>c.FeatureComponent)
+    }]
+     },
+     {
+        path:'**',
+        redirectTo: 'home',
+     }
+  
 ];
